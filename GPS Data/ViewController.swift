22ec18086ff12d0f83d1ddf2b9da91894d7f42f0
Maciej Gad <gad.maciej@gmail.com>
2015-01-01 20:46:17 +0100

    Add new project

---
+++ GPS Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPS Data
+//
+//  Created by Maciek on 01.01.2015.
+//  Copyright (c) 2015 Maciej Gad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
